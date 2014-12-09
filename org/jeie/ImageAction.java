70ac9b07fa15a8a1def3f03a92f0581bfbf2c139
Robert B Colton <robertbcolton@gmail.com>
2014-12-08 22:13:07 -0500

    Update all copyright headers to doxygen and provide descriptions.

--- org/jeie/ImageAction.java
+++ org/jeie/ImageAction.java
@@ -1,22 +1,27 @@
-/*
- * Copyright (C) 2008, 2012 IsmAvatar <IsmAvatar@gmail.com>
- * Copyright (C) 2013 jimn346 <jds9496@gmail.com>
- * 
- * This file is part of Jeie.
- * 
- * Jeie is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Jeie is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License (COPYING) for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+/**
+* @file  ImageAction.java
+* @brief Collection of various repeatable image edit actions that can be undone.
+*
+* @section License
+*
+* Copyright (C) 2008, 2012 IsmAvatar <IsmAvatar@gmail.com>
+* Copyright (C) 2013 jimn346 <jds9496@gmail.com>
+* 
+* This file is a part of JEIE.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
 
 package org.jeie;
 
